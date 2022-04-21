4445a04896fe12b1198408dd2484ad4910d05ffa
Goularde <celianmaurin@hotmail.com>
2022-04-21 16:30:30 +0200

    communication bdd

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,29 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [nomArgonaute, setNomArgonaute] = useState("");
+  //let nomArgonaute = "";
+
+  const submitNomArgonaute = (e) => {
+    Axios.post("http://localhost:3001/api/insert", {
+      nomArgonaute: nomArgonaute,
+    })
+      .then(() => {
+        alert("Ajout effectué");
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(() => console.log("end request"));
+  };
+  // finally s'exécute tout le temps à la fin d'une requete que tu recoive une erreur ou des données
+  // function handleChange(event) {
+  //   nomArgonaute = event.target.value;
+  //   console.log(event.target.value);
+  // }
+
   return (
     <div className="App">
       <header>
@@ -16,9 +39,20 @@ function App() {
       <main>
         <h2>Ajouter un(e) Argonaute</h2>
         <form class="new-member-form">
-          <label for="name">Nom de l&apos;Argonaute</label>
-          <input id="name" name="name" type="text" placeholder="Charalampos" />
-          <button type="submit">Envoyer</button>
+          <label for="name">Nom de l'Argonaute</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Charalampos"
+            onChange={
+              //handleChange
+              (e) => {
+                setNomArgonaute(e.target.value);
+              }
+            }
+          />
+          <button onClick={submitNomArgonaute}>Envoyer</button>
         </form>
 
         <h2>Membres de l'équipage</h2>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const db = mysql.createPool({
   host: "localhost",
@@ -9,7 +11,17 @@ const db = mysql.createPool({
   database: "argodatabase",
 });
 
-app.get("/", (req, res) => {});
+app.use(cors);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/insert", (req, res) => {
+  const nomArgonaute = req.body.nomArgonaute;
+  console.log(nomArgonaute);
+
+  const sqlInsert = "INSERT INTO argonaute (nomArgonaute) VALUES (?)";
+  db.query(sqlInsert, [nomArgonaute], (err, result) => {});
+  console.log(err);
+});
 
 app.listen(3001, () => {
   console.log("Running on port 3001");
